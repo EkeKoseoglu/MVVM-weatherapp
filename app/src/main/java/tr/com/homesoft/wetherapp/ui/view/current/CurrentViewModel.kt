47d75d43b9195ee45c087026ce9833404b51b88f
Eke Koseoglu <eke.kose@gmail.com>
2018-12-20 00:38:22 +0300

    Constructor parameter reformatted

--- app/src/main/java/tr/com/homesoft/wetherapp/ui/view/current/CurrentViewModel.kt
+++ app/src/main/java/tr/com/homesoft/wetherapp/ui/view/current/CurrentViewModel.kt
@@ -3,7 +3,7 @@ package tr.com.homesoft.wetherapp.ui.view.current
 import androidx.lifecycle.ViewModel
 import tr.com.homesoft.wetherapp.data.repository.ForecastRepository
 
-class CurrentViewModel(private val repository: ForecastRepository) : ViewModel() {
+class CurrentViewModel(repository: ForecastRepository) : ViewModel() {
 
     //internal suspend fun getCurrentWeather() = repository.getCurrentWeather()
 
