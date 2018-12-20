066bd281715b8ed560695df028282a243963aef1
Eke Koseoglu <eke.kose@gmail.com>
2018-12-20 19:36:49 +0300

    Databinding is implemented

--- app/src/main/java/tr/com/homesoft/wetherapp/ui/view/MainActivity.kt
+++ app/src/main/java/tr/com/homesoft/wetherapp/ui/view/MainActivity.kt
@@ -2,23 +2,30 @@ package tr.com.homesoft.wetherapp.ui.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import kotlinx.android.synthetic.main.activity_main.*
 import tr.com.homesoft.wetherapp.R
+import tr.com.homesoft.wetherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
+    }
 
     private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        setSupportActionBar(toolbar)
+        setSupportActionBar(binding.toolbar)
 
         setupBottomNav(navController)
 
@@ -26,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupBottomNav(navController: NavController) {
         with(navController) {
-            bottom_nav.setupWithNavController(this)
+            binding.bottomNav.setupWithNavController(this)
             setupActionBarWithNavController(this, null)
         }
     }
