2e8cbda8d4c30ce7a567137f37d262cef5287a46
Eke Koseoglu <eke.kose@gmail.com>
2018-12-23 12:31:15 +0300

    Threetenabp implemented

--- app/src/main/java/tr/com/homesoft/wetherapp/ForecastApp.kt
+++ app/src/main/java/tr/com/homesoft/wetherapp/ForecastApp.kt
@@ -1,6 +1,11 @@
 package tr.com.homesoft.wetherapp
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.startKoin
 import tr.com.homesoft.wetherapp.di.appModule
 
@@ -8,6 +13,9 @@ class ForecastApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        CoroutineScope(Dispatchers.IO).launch {
+            AndroidThreeTen.init(this@ForecastApp)
+        }
         startKoin(this, listOf(appModule))
     }
 }
