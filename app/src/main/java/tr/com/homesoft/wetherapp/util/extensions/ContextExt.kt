c2852561685a52ad886c91166e2790eff8303325
Eke Koseoglu <eke.kose@gmail.com>
2018-12-25 15:23:39 +0300

    renamed from Extensions.kt

---
+++ app/src/main/java/tr/com/homesoft/wetherapp/util/extensions/ContextExt.kt
@@ -0,0 +1,25 @@
+package tr.com.homesoft.wetherapp.util.extensions
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.view.LayoutInflater
+import androidx.core.content.ContextCompat
+
+
+fun Context.isPermissionGranted(permission: String) =
+    ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+
+/**
+ * Extension method to provide quicker access to the [LayoutInflater] from [Context].
+ */
+fun Context.getLayoutInflater() = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+/**
+ * Create an intent for [T] and apply a lambda on it
+ */
+inline fun <reified T : Any> Context.intent(body: Intent.() -> Unit): Intent {
+    val intent = Intent(this, T::class.java)
+    intent.body()
+    return intent
+}
