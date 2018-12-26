89b50c833856f63d1f6e635e9a4cd870047e8af2
Eke Koseoglu <eke.kose@gmail.com>
2018-12-27 00:28:30 +0300

    Code reformatted in accordance with Core-KTX

--- app/src/main/java/tr/com/homesoft/wetherapp/data/remote/ConnectivityInterceptorImpl.kt
+++ app/src/main/java/tr/com/homesoft/wetherapp/data/remote/ConnectivityInterceptorImpl.kt
@@ -2,7 +2,6 @@ package tr.com.homesoft.wetherapp.data.remote
 
 import android.content.Context
 import android.net.ConnectivityManager
-import androidx.core.content.getSystemService
 import okhttp3.Interceptor
 import okhttp3.Response
 import tr.com.homesoft.wetherapp.data.remote.internal.NoConnectionException
@@ -18,8 +17,8 @@ class ConnectivityInterceptorImpl(context: Context) : ConnectivityInterceptor {
     }
 
     private fun isConnected(): Boolean {
-        val connManager =  appContext.getSystemService<ConnectivityManager>() //appContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkInfo = connManager?.activeNetworkInfo
+        val connManager = appContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connManager.activeNetworkInfo
         return networkInfo != null && networkInfo.isConnected
     }
 }
