e20da880db7a401bcc334f6ed3c7f3c030e95e37
Eke Koseoglu <eke.kose@gmail.com>
2018-12-27 12:16:36 +0300

    Exceptions gathered in one class

---
+++ app/src/main/java/tr/com/homesoft/wetherapp/data/remote/internal/Exceptions.kt
@@ -0,0 +1,6 @@
+package tr.com.homesoft.wetherapp.data.remote.internal
+
+import java.io.IOException
+
+class Exceptions : IOException()
+class LocationPermissionNotGrantedException : Exception()
--- app/src/main/java/tr/com/homesoft/wetherapp/data/remote/internal/LocationPermissionNotGrantedException.kt
+++
@@ -1,6 +0,0 @@
-package tr.com.homesoft.wetherapp.data.remote.internal
-
-import java.lang.Exception
-
-class LocationPermissionNotGrantedException: Exception() {
-}
