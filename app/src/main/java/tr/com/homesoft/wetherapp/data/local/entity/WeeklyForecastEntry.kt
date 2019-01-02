fda59c52b5c3f8ebd9e27e39ffe64b7bd31c8ee6
Eke Koseoglu <eke.kose@gmail.com>
2019-01-02 12:19:11 +0300

    DateConverter added

--- app/src/main/java/tr/com/homesoft/wetherapp/data/local/entity/WeeklyForecastEntry.kt
+++ app/src/main/java/tr/com/homesoft/wetherapp/data/local/entity/WeeklyForecastEntry.kt
@@ -1,14 +1,14 @@
 package tr.com.homesoft.wetherapp.data.local.entity
 
-import androidx.room.Embedded
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
 import com.google.gson.annotations.SerializedName
+import tr.com.homesoft.wetherapp.data.local.converter.DateConverter
 import tr.com.homesoft.wetherapp.data.local.entity.DayForecast.Companion.DAYFORECAST
 import tr.com.homesoft.wetherapp.data.local.entity.WeeklyForecastEntry.Companion.WEEKLY_FORECAST_TABLE_NAME
 
 @Entity(tableName = WEEKLY_FORECAST_TABLE_NAME)
 data class WeeklyForecastEntry(
+    @TypeConverters(DateConverter::class)
     val date: String,
     @PrimaryKey
     @SerializedName("date_epoch")
