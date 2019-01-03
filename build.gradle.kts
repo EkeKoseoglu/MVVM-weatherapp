5aa6c08d2609faf27676aa29593e9e5f8fd84c93
Eke Koseoglu <eke.kose@gmail.com>
2019-01-03 09:13:44 +0300

    Migrating grrovy gradle to kotlin gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,21 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.11'
+    extra["kotlin_version"] = "1.3.11"
+
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.2.1'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-        classpath 'android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0-alpha09'
+        classpath("com.android.tools.build:gradle:3.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlin_version"]}")
+        classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0-alpha09")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
+
 
 allprojects {
     repositories {
@@ -22,6 +24,7 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+val clean by tasks.creating(Delete::class) {
+    delete(rootProject.buildDir)
 }
+
