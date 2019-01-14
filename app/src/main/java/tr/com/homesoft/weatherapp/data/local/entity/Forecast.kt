32bcf7a5c7b809807e0c2c75882b12c23d26ae98
Eke Koseoglu <eke.kose@gmail.com>
2019-01-14 12:39:59 +0300

    Code reformatted

--- app/src/main/java/tr/com/homesoft/weatherapp/data/local/entity/Forecast.kt
+++ app/src/main/java/tr/com/homesoft/weatherapp/data/local/entity/Forecast.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class Forecast(
     @SerializedName("forecastday")
-    val weeklyForcastList: List<WeeklyForecastEntry>
+    val weeklyForecastList: List<WeeklyForecastEntry>
 )
