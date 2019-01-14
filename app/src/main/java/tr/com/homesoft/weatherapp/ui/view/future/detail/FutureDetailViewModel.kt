adb9f52e2389674d25f55c12230fd827deb53a96
Eke Koseoglu <eke.kose@gmail.com>
2019-01-14 11:57:31 +0300

    Code reformatted

--- app/src/main/java/tr/com/homesoft/weatherapp/ui/view/future/detail/FutureDetailViewModel.kt
+++ app/src/main/java/tr/com/homesoft/weatherapp/ui/view/future/detail/FutureDetailViewModel.kt
@@ -5,40 +5,20 @@ import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import kotlinx.coroutines.launch
-import tr.com.homesoft.weatherapp.data.local.entity.WeatherLocation
 import tr.com.homesoft.weatherapp.data.local.unitlocalized.weekly.UnitSpecificWeeklyForecastEntry
 import tr.com.homesoft.weatherapp.data.provider.UnitProvider
 import tr.com.homesoft.weatherapp.data.repository.ForecastRepository
-import tr.com.homesoft.weatherapp.ui.base.ScopedViewModel
-import tr.com.homesoft.weatherapp.ui.unitsystem.UnitSystem
+import tr.com.homesoft.weatherapp.ui.base.WeatherViewModel
 
-class FutureDetailViewModel(private val repository: ForecastRepository, unitProvider: UnitProvider) : ScopedViewModel() {
+class FutureDetailViewModel(private val repository: ForecastRepository, unitProvider: UnitProvider) :
+    WeatherViewModel(unitProvider, repository) {
 
-    private val unitSystem = MutableLiveData<UnitSystem>()
-
-    val metric: LiveData<Boolean> = Transformations.map(unitSystem) { it == UnitSystem.METRIC }
-
-    val weatherLocation: LiveData<WeatherLocation>  = getLocation()
-
-    private fun getLocation(): LiveData<WeatherLocation> {
-        val location = MediatorLiveData<WeatherLocation>()
-        launch {
-            val result = repository.getWeatherLocation()
-            location.addSource(result) {
-                location.postValue(it)
-                location.removeSource(result)
-            }
-        }
-        return location
-    }
-
-    val loading = MutableLiveData<Boolean>()
 
     private val mDetailForecast = MutableLiveData<UnitSpecificWeeklyForecastEntry>()
     val detailForecast: LiveData<UnitSpecificWeeklyForecastEntry> get() = mDetailForecast
 
     internal fun getDetailForecastByDate(date: String): LiveData<UnitSpecificWeeklyForecastEntry> =
-        Transformations.switchMap(metric) { getWeatherByDate(date, it) }
+        Transformations.switchMap(isMetric) { getWeatherByDate(date, it) }
 
 
     private fun getWeatherByDate(date: String, isMetric: Boolean): LiveData<UnitSpecificWeeklyForecastEntry> {
@@ -53,11 +33,9 @@ class FutureDetailViewModel(private val repository: ForecastRepository, unitProv
         return result
 
     }
+
     internal fun setDetailForecast(data: UnitSpecificWeeklyForecastEntry) {
         mDetailForecast.value = data
     }
 
-    init {
-        unitSystem.value = unitProvider.getUnitSystem()
-    }
 }
